dec74bb22cf40eac293b1d321026f7d1ab878f8a
hesuxin <shuxin.he@socialcredits.cn>
2025-04-15 10:18:27 +0800

    update bevy_egui to 0.34.0-rc

--- crates/bevy-inspector-egui/src/bevy_inspector/hierarchy.rs
+++ crates/bevy-inspector-egui/src/bevy_inspector/hierarchy.rs
@@ -68,7 +68,7 @@ impl<T> Hierarchy<'_, T> {
             .iter()
             .flat_map(|selected| {
                 std::iter::successors(Some(selected), |&entity| {
-                    self.world.get::<ChildOf>(entity).map(|c| c.parent)
+                    self.world.get::<ChildOf>(entity).map(|c| c.parent())
                 })
                 .skip(1)
             })
